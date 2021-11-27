d6f3aca0017a5f2e7a1d22fdac23dfd81bc66f5b
gandor999 <geodorruales@gmail.com>
2021-11-27 14:55:30 +0800

    Add points and who wins

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment, useState, useEffect } from 'react';
-import { Form, Button, Table } from 'react-bootstrap';
+import { Form, Button, Table, Modal } from 'react-bootstrap';
 import checkMark from './images/checkMark.png';
 import wrongMark from './images/wrongMark.png';
 import circle from './images/circle.png';
@@ -16,9 +16,15 @@ function App() {
     const [xTurn, setXTurn] = useState(true);
     const [oTurn, setOTurn] = useState(false);
     const [whoWin, setWhoWin] = useState('');
+    const [xPoint, setXPoint] = useState(0);
+    const [oPoint, setOPoint] = useState(0);
+    const [timesClicked, setTimesClicked] = useState(0);
 
     console.log(`X turn: ${xTurn}`)
     console.log(`O turn: ${oTurn}`)
+    console.log(`xpoint: ${xPoint}`)
+    console.log(`opoint: ${oPoint}`)
+    console.log(`timesClicked: ${timesClicked}`)
 
     const [show, setShow] = useState(false);
 
@@ -54,6 +60,8 @@ function App() {
 
 
     useEffect(() => {
+
+      // When X wins
       if(
         (xr1c1.status && xr1c2.status && xr1c3.status) ||
         (xr2c1.status && xr2c2.status && xr2c3.status) ||
@@ -68,9 +76,55 @@ function App() {
       ){setWhoWin('x')}
 
 
+      if(
+        (xr3c3.status && xr2c2.status && xr1c1.status) ||
+        (xr3c1.status && xr2c2.status && xr1c3.status)
+      ){setWhoWin('x')}
+
+
+      // When O wins
+      if(
+        (or1c1.status && or1c2.status && or1c3.status) ||
+        (or2c1.status && or2c2.status && or2c3.status) ||
+        (or3c1.status && or3c2.status && or3c3.status)
+      ){setWhoWin('o')}
+
+
+      if(
+        (or3c1.status && or2c1.status && or1c1.status) ||
+        (or3c2.status && or2c2.status && or1c2.status) ||
+        (or3c3.status && or2c3.status && or1c3.status)
+      ){setWhoWin('o')}
+
+
+      if(
+        (or3c3.status && or2c2.status && or1c1.status) ||
+        (or3c1.status && or2c2.status && or1c3.status)
+      ){setWhoWin('o')}
+
+      setTimeout(() => {
+        if(timesClicked == 9 && whoWin != 'x' && whoWin != 'o'){setWhoWin('draw')}
+      }, 1000)
         
 
     }, [xTurn, oTurn])
+
+
+    useEffect(() => {
+      if(whoWin == 'x'){
+        setXPoint(xPoint + 1);
+        handleShow();
+      }
+
+      if(whoWin == 'o'){
+        setOPoint(oPoint + 1);
+        handleShow();
+      }
+
+      if(whoWin == 'draw'){
+        handleShow();
+      }
+    }, [whoWin])
 
     console.log(whoWin);
 
@@ -90,13 +144,51 @@ function App() {
           setO({status: true, isClicked: true});
           setXTurn(true);
           setOTurn(false);
-        }        
+        }
+
+        setTimesClicked(timesClicked + 1);      
       }
+    }
+
+
+    function playAgain(){
+      handleClose();
+
+      // Close all X
+      setXR1C1({status: false, isClicked: false});
+      setXR1C2({status: false, isClicked: false});
+      setXR1C3({status: false, isClicked: false});
+
+      setXR2C1({status: false, isClicked: false});
+      setXR2C2({status: false, isClicked: false});
+      setXR2C3({status: false, isClicked: false});
+
+      setXR3C1({status: false, isClicked: false});
+      setXR3C2({status: false, isClicked: false});
+      setXR3C3({status: false, isClicked: false});
+
+
+      // Close all O
+      setOR1C1({status: false, isClicked: false});
+      setOR1C2({status: false, isClicked: false});
+      setOR1C3({status: false, isClicked: false});
+
+      setOR2C1({status: false, isClicked: false});
+      setOR2C2({status: false, isClicked: false});
+      setOR2C3({status: false, isClicked: false});
+
+      setOR3C1({status: false, isClicked: false});
+      setOR3C2({status: false, isClicked: false});
+      setOR3C3({status: false, isClicked: false});
+
+      setTimesClicked(0);
+
+      setWhoWin('');
     }
   
 
   return (
-      <div className="d-flex table-wrapper justify-content-center align-items-center mt-5">
+      <div className="d-flex flex-column table-wrapper justify-content-center align-items-center mt-5 p-5">
         <Table borderless className="w-25">
           
           <tbody className="text-center">
@@ -210,6 +302,31 @@ function App() {
             
           </tbody>
         </Table>
+
+        <div className="d-flex">
+          <h4 className="p-5">X Score: {xPoint}</h4>
+          <h4 className="p-5">O Score: {oPoint}</h4>
+        </div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header className="text-center">
+            {
+              (whoWin == 'x') ?
+                <Modal.Title>X Wins!</Modal.Title>
+              :
+                (whoWin == 'o') ?
+                  <Modal.Title>O Wins!</Modal.Title>
+                :
+                  <Modal.Title>Draw</Modal.Title>
+            }
+          </Modal.Header>
+          
+          <Modal.Footer className="text-center">
+            <Button variant="warning" onClick={playAgain}>
+              Another Round
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
       
   )
