14f097307d1b4d54bd47ebbb91ac836c47d426ac
t-alvamo <moises.alvarado@autodesk.com>
2021-12-07 12:54:07 -0600

    initial state machine (shell)

--- src/machine.js
+++ src/machine.js
@@ -1,8 +1,77 @@
 import { createMachine } from 'xstate';
 
-const ticTacToeMachine = createMachine(
-  {
+const ticTactToeMachine = createMachine({
+  id: 'ticTactToe',
+  initial: 'onGame',
+  context: {
+    score: {
+      x: 0,
+      o: 0
+    },
+    whosPlaying: 'x', // Values X or O
+    lastWinner: null,
+    board: Array(9).fill(null)
+  },
+  states: {
+    onGame: {
+      on: {
+        ONCLICK: [
+          {
+            cond: 'evaluateWin',
+            target: 'win'
+          },
+          {
+            cond: 'evaluateDraw',
+            target: 'draw'
+          },
+          {
+            target: 'onGame'
+          }
+        ]
+      },
+      exit: 'updateBoard',
+      meta: {
+        message: 'hola wey'
+      }
+    },
+    win: {
+      on: {
+        anotherRound: {
+          target: 'onGame'
+        }
+      },
+      exit: ['updateScore', 'resetBoard']
+    },
+    draw: {
+      on: {
+        anotherRound: {
+          target: 'onGame'
+        }
+      },
+      exit: ['resetBoard']
+    }
   }
-);
+},
+{
+  guards: {
+    evaluateWin: (context) => {
+      return false;
+    },
+    evaluateDraw: (context) => {
+      return false;
+    }
+  },
+  actions: {
+    updateScore: (context, event) => {
+      // score update must be here :P
+    },
+    resetBoard: (context, event) => {
+      // Clean all the context varaibles in order to start a new game board
+    },
+    updateBoard: (context, event) => {
+      // who played and what was the play?
+    }
+  }
+});
 
-export { ticTacToeMachine };
+export { ticTactToeMachine };
